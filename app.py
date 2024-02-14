37250593745e31675cd1e9243bd339faae305aef
FrazierMark <m.mark.frazier@gmail.com>
2024-02-14 10:55:08 -0800

    Added migrations in order to add users

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from grocery_app.extensions import app, db
 from grocery_app.routes import main
+from flask_migrate import Migrate
 
 app.register_blueprint(main)
 
 with app.app_context():
     db.create_all()
+    migrate = Migrate(app, db)
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
